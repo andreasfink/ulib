ebf7dfae9106d9e3d9a71e4d764178e39d39a673
Andreas Fink <andreas@fink.org>
2017-03-30 15:22:28 +0200

    changes for Linux makefiles

--- Classes/UniversalTokenizer/UMSyntaxToken_Digits.h
+++ Classes/UniversalTokenizer/UMSyntaxToken_Digits.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Andreas Fink (andreas@fink.org). All rights reserved.
 //
 
-#import <ulib/ulib.h>
+#import "UMSyntaxToken.h"
 
 @interface UMSyntaxToken_Digits : UMSyntaxToken
 {

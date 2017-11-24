80dc3f429a457816266ea9e40a9169e79b8ac7fb
Andreas Fink <andreas@fink.org>
2017-11-24 11:57:32 +0100

    updated test cases.

---
+++ ulibTests/TestUMPool.h
@@ -0,0 +1,13 @@
+//
+//  TestUMPool.h
+//  ulibTests
+//
+//  Created by Andreas Fink on 24.11.17.
+//  Copyright © 2017 Andreas Fink (andreas@fink.org). All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestUMPool : XCTestCase
+
+@end
--- ulibTests/TestUMQueue.m
+++ ulibTests/TestUMQueue.m
@@ -37,28 +37,6 @@
         [queue getFirst];
         XCTAssertTrue([queue count] == 0, @"all elements removed from queue");
         
-        UMQueue *unlockedQueue = [[UMQueue alloc] initWithoutLock];
-        [unlockedQueue lock];
-        [unlockedQueue append:@"first"];
-        [unlockedQueue unlock];
-        [unlockedQueue lock];
-        [unlockedQueue append:@"second"];
-        [unlockedQueue unlock];
-        [unlockedQueue lock];
-        [unlockedQueue append:@"third"];
-        [unlockedQueue unlock];
-        XCTAssertTrue([unlockedQueue count] == 3, @"unlocked queue should have 3 elements");
-        [unlockedQueue lock];
-        NSString *unlockedFirst = [unlockedQueue getFirst];
-        [unlockedQueue unlock];
-        XCTAssertTrue([unlockedFirst compare:@"first"] == NSOrderedSame, @"first element added to unlocked queue was first");
-        [unlockedQueue lock];
-        [unlockedQueue getFirst];
-        [unlockedQueue unlock];
-        [unlockedQueue lock];
-        [unlockedQueue getFirst];
-        [unlockedQueue unlock];
-        XCTAssertTrue([unlockedQueue count] == 0, @"all elements removed from unlocked queuue");
     
     }
 }

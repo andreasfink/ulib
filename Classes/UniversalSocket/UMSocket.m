3f822c4d21677643bfbc6fd90c48e4af6151a28a
Andreas Fink <andreas@fink.org>
2018-08-05 11:56:36 +0200

    work in progress

--- Classes/UniversalSocket/UMSocket.m
+++ Classes/UniversalSocket/UMSocket.m
@@ -2890,7 +2890,7 @@ int send_usrsctp_cb(struct usocket *sock, uint32_t sb_free)
         SSL_library_init();
         SSLeay_add_ssl_algorithms();
         SSL_load_error_strings();
-#ifdef  HAVE_TLS_METHOD
+#if  defined(HAVE_TLS_METHOD) || defined(__APPLE__)
         global_generic_ssl_context = SSL_CTX_new(TLS_method());
         global_server_ssl_context = SSL_CTX_new(TLS_server_method());
         global_client_ssl_context = SSL_CTX_new(TLS_client_method());

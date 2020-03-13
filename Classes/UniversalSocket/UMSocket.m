c12d3094a2321b059d82946d80a4b5421c9c098a
Andreas Fink <andreas@fink.org>
2020-03-13 08:10:38 +0100

    minor

--- Classes/UniversalSocket/UMSocket.m
+++ Classes/UniversalSocket/UMSocket.m
@@ -532,8 +532,6 @@ static int SSL_smart_shutdown(SSL *ssl)
 
 - (UMSocketError) bind
 {
-    //UMAssert(_requestedLocalPort != 0,@"bind to port 0 not allowed");
-
     [_controlLock lock];
     @try
     {

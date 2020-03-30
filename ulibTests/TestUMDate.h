0cff9fc4f7c996d4a95cf16721719dde19630e68
Andreas Fink <andreas@fink.org>
2020-03-30 12:16:47 +0200

    fix cdr writer typo

---
+++ ulibTests/TestUMDate.h
@@ -0,0 +1,17 @@
+//
+//  TestUMDate.h
+//  ulibTests
+//
+//  Created by Andreas Fink on 30.03.20.
+//  Copyright © 2020 Andreas Fink (andreas@fink.org). All rights reserved.
+//
+
+#import <ulib/ulib.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestUMDate : UMObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ulibTests/TestUMDate.m
@@ -0,0 +1,13 @@
+//
+//  TestUMDate.m
+//  ulibTests
+//
+//  Created by Andreas Fink on 30.03.20.
+//  Copyright © 2020 Andreas Fink (andreas@fink.org). All rights reserved.
+//
+
+#import "TestUMDate.h"
+
+@implementation TestUMDate
+
+@end

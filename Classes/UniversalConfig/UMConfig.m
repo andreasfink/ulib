701daf180aa4cc460d19a627403ecc0dfbc0d682
Andreas Fink <andreas@fink.org>
2021-01-13 19:02:43 +0000

    minor

--- Classes/UniversalConfig/UMConfig.m
+++ Classes/UniversalConfig/UMConfig.m
@@ -436,9 +436,6 @@ extern NSString *UMBacktrace(void **stack_frames, size_t size);
     {
         return str;
     }
-<<<<<<< HEAD
     return str;
-=======
->>>>>>> 925140de3fe96456cb396913f2b982fecf67ced9
 }
 @end

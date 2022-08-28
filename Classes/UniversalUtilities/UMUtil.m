ab80fca78451fa421653cf7840fe28eccf4721c0
Andreas Fink <andreas@fink.org>
2022-08-28 23:30:32 +0300

    remove warnings

--- Classes/UniversalUtilities/UMUtil.m
+++ Classes/UniversalUtilities/UMUtil.m
@@ -719,7 +719,6 @@ static NSArray *        _machineCPUIDs = NULL;
 
 +(NSArray *)readChildProcess:(NSArray *)args
 {
-    int errnoOccured=0;
     int pipefds[2];
     pid_t pid;
     NSMutableArray *result = NULL;
@@ -754,7 +753,6 @@ static NSArray *        _machineCPUIDs = NULL;
         if (execvp(cmd[0], cmd) == -1)
         {
 			fprintf(stderr,"execvp(%s) fails with errno %d %s",cmd[0],errno,strerror(errno));
-            errnoOccured = errno;
         }
         /* we actually should never get here.*/
         exit(0);

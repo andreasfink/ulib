c1c5eec7cf9c132d6637d82be9fd7a1939353ff5
Andreas Fink <andreas@fink.org>
2025-06-20 14:52:57 +0200

    wip

--- ulib/ulib.h
+++ ulib/ulib.h
@@ -131,7 +131,7 @@ typedef enum
 #import <ulib/UMThreadHelpers.h>
 #import <ulib/UMCommandLine.h>
 #import <ulib/UMProtocolBuffer.h>
-#import <ulib/UMNamedListOld.h>
+#import <ulib/UMNamedListOld.h> // the new implementation is now in ulibasn1
 #import <ulib/UMStatistic.h>
 #import <ulib/UMStatisticEntry.h>
 #import <ulib/UMRegexMatch.h>
